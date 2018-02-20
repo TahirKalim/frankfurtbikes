5d70cef2cd38aef9ef066c85846c5d1ea909233a
tahirkalim <thrkalim@gmail.com>
2018-02-20 03:18:56 +0100

    Add links to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   resources :products
   get 'simple_pages/index'
 
@@ -6,8 +7,8 @@ Rails.application.routes.draw do
 
   get 'simple_pages/contact'
 
-  root 'simple_pages#landing_page'
-  
+  #root 'simple_pages#landing_page'
+  root 'products#index'
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
